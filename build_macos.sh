de3569d1f224d733658eafd46a845020cdc2b3d6
Valentyn Sobol <napster_1997@mail.ru>
2025-09-22 22:52:54 +0300

    update

--- build_macos.sh
+++ build_macos.sh
@@ -1,7 +1,13 @@
+export MACOSX_DEPLOYMENT_TARGET=11.1
+#apt-get install -y openjdk-8-jdk
+#omp install openjdk11
+
 apt-get install -y autoconf gperf
 
 export CC=oa64-clang
 export CXX=oa64-clang++
+
+rm -rf dist-mac
 
 autoconf
 
@@ -16,6 +22,17 @@ export LDFLAGS="-L/libgmp_static/dist/lib"
     --with-pic-gmp=/libgmp_static/dist/lib/libgmp.a \
     --with-pic-gmp-include-dir=/libgmp_static/dist/include
 
-make MODE=release ARCH=arm64-apple-darwin20.2 POSIXOS=darwin show-details dist install
+make MODE=release ARCH=arm64-apple-darwin20.2 POSIXOS=darwin show-details clean dist install
 
-llvm-install-name-tool-14 -id libyices.2.dylib dist-mac/lib/libyices.2.dylib
+llvm-install-name-tool-18 -id libyices.2.dylib dist-mac/lib/libyices.2.dylib
+
+mkdir -p dist-mac/dist
+cp dist-mac/lib/libyices.2.dylib dist-mac/dist/libyices.dylib
+
+cd java-bindings
+INSTALL_DIR=/mnt/dist-mac/dist ./build_macos.sh
+cd ..
+
+llvm-install-name-tool-18 -id libyices2java.dylib dist-mac/dist/libyices2java.dylib
+
+llvm-otool-18 -L dist-mac/dist/*
